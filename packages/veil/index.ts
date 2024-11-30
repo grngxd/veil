1bae0e3571be56af3bba3bb06b48ad956c6c9432
grngxd <36968271+grngxd@users.noreply.github.com>
2024-11-30 13:27:23 +0000

    increment ver

--- packages/veil/index.ts
+++ packages/veil/index.ts
@@ -1,7 +1,7 @@
 import { generate } from "short-uuid";
 
-const version = "1.3-A4";
-const semver = "1.3.4";
+const version = "1.3-A5";
+const semver = "1.3.5";
 
 const context = generate();
 const proxyPort = 4443;
